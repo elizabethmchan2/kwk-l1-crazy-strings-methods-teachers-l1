026562facf4b2061072ccd89174fff1a4b0bf213
Elizabeth Chan <lmchan@brynmawr.edu>
2018-07-02 17:51:00 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,5 +1,11 @@
 # write your method here
 
 def crazy_strings(a,b)
-
+  puts "#{a.reverse.upcase}"
+  if !b.empty?
+  # or unless b.empty
+    puts "#{b.swapcase.gsub("s","z").gsub("S","Z")}"
+  end
 end
+
+assert(crazy_strings("APPLE", "") == "ELPPA")
